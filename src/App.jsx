b7c1139cf849b04a9bb2b3a0554bc8f091c8dfa0
Farsana PH <farsanaph2001@gmail.com>
2025-08-29 22:45:47 +0530

    updated gradepage

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,7 @@ import GradePage from "./common/GradePage";
 import KidsHomePage from "./user/KidsHomePage"
 
 function App() {
-<<<<<<< HEAD
-  return <KidsHomePage/>;
-=======
-  return (
-    <>
-      {/* <GradePage />; */}
-      <KidsHomePage />
-    </>
-  )
->>>>>>> e559200857c9cce3b8acbf54892737051b708338
+ return <GradePage/>;
 }
 
 export default App;
--- src/common/GradePage.jsx
+++ src/common/GradePage.jsx
@@ -1,87 +1,77 @@
 import React from "react";
 import "./GradePage.css";
 
-<<<<<<< HEAD:src/common/Home.jsx
-const HomePage = () => {
-    const boards = [...Array(8)];
+const GradePage = () => {
+  // Array of 8 different images
+  const boards = [
+    "/assets/GRADE1.png",
+    "/assets/grade2.png",
+    "/assets/GRADE3.png",
+    "/assets/GRADE4.png",
+    "/assets/GRADE5.png",
+    "/assets/GRADE6.png",
+    "/assets/GRADE7.png",
+    "/assets/GRADE8.png",
+  ];
 
-    return (
-        <div className="homepage">
-            <div className="container text-center">
-
-                <div className="d-none d-md-block d-lg-none">
-                    <div className="row justify-content-center mb-4">
-                        {boards.slice(0, 3).map((_, index) => (
-                            <div key={index} className="col-4 d-flex justify-content-center">
-                                <img src="/assets/class board.png" alt="Board" className="img-fluid board" />
-                            </div>
-                        ))}
-                    </div>
-                    <div className="row justify-content-center mb-4">
-                        {boards.slice(3, 5).map((_, index) => (
-                            <div key={index + 3} className="col-4 d-flex justify-content-center">
-                                <img src="/assets/class board.png" alt="Board" className="img-fluid board" />
-                            </div>
-                        ))}
-                    </div>
-                    <div className="row justify-content-center">
-                        {boards.slice(5, 8).map((_, index) => (
-                            <div key={index + 5} className="col-4 d-flex justify-content-center">
-                                <img src="/assets/class board.png" alt="Board" className="img-fluid board" />
-                            </div>
-                        ))}
-                    </div>
-                </div>
-
-                <div className="row g-1 d-md-none d-lg-flex">
-                    {boards.map((_, index) => (
-                        <div
-                            key={index}
-                            className="col-6 col-md-3 d-flex justify-content-center mb-4"
-                        >
-                            <img
-                                src="/assets/class board.png"
-                                alt="Board"
-                                className="img-fluid board"
-                            />
-                        </div>
-                    ))}
-                </div>
-=======
-function GradePage() {
   return (
-    <div
-      className="relative w-full h-screen"
-      style={{
-        backgroundImage: "url('/assets/Page 1.png')",
-        backgroundSize: "100% 100%",
-        backgroundPosition: "center",
-        paddingBottom: "22vh",
-        backgroundRepeat: "no-repeat",
-      }}
-    >
+    <div className="homepage">
       <div className="container text-center">
-        <div className="row g-1">
-          {[...Array(8)].map((_, index) => (
+        {/* Medium devices (md) only */}
+        <div className="d-none d-md-block d-lg-none">
+          <div className="row justify-content-center mb-4">
+            {boards.slice(0, 3).map((img, index) => (
+              <div key={index} className="col-4 d-flex justify-content-center">
+                <img
+                  src={img}
+                  alt={`Board ${index + 1}`}
+                  className="img-fluid board"
+                />
+              </div>
+            ))}
+          </div>
+          <div className="row justify-content-center mb-4">
+            {boards.slice(3, 5).map((img, index) => (
+              <div key={index + 3} className="col-4 d-flex justify-content-center">
+                <img
+                  src={img}
+                  alt={`Board ${index + 4}`}
+                  className="img-fluid board"
+                />
+              </div>
+            ))}
+          </div>
+          <div className="row justify-content-center">
+            {boards.slice(5, 8).map((img, index) => (
+              <div key={index + 5} className="col-4 d-flex justify-content-center">
+                <img
+                  src={img}
+                  alt={`Board ${index + 6}`}
+                  className="img-fluid board"
+                />
+              </div>
+            ))}
+          </div>
+        </div>
+
+        {/* Small & large screens */}
+        <div className="row g-1 d-md-none d-lg-flex">
+          {boards.map((img, index) => (
             <div
               key={index}
               className="col-6 col-md-3 d-flex justify-content-center mb-4"
             >
               <img
-                src="/assets/class board.png"
-                alt="Board"
-                className="img-fluid"
-                style={{
-                  marginTop: "10vw",
-                  width: "25vw", // default size
-                  maxWidth: "120px", // keeps it from being too large
-                  objectFit: "contain",
-                }}
+                src={img}
+                alt={`Board ${index + 1}`}
+                className="img-fluid board"
               />
->>>>>>> e559200857c9cce3b8acbf54892737051b708338:src/common/GradePage.jsx
             </div>
+          ))}
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default GradePage;
